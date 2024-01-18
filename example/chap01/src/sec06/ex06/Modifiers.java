2569836125a26242c898c14d8587cebacc1f4a5a
excelhsh <excelh@kakao.com>
2024-01-18 14:43:23 +0900

    .

--- example/chap01/src/sec06/ex06/Modifiers.java
+++ example/chap01/src/sec06/ex06/Modifiers.java
@@ -1,24 +1,25 @@
 package sec06.ex06;
 
-public class Modifiers {
-		//필드
-		public int field1;			
-		int field2;				
-		private int field3;			
+public class modifiers {
+
+	//필드
+	public int field1;			//public 접근 제한자
+	int field2;						//default 접근 제한자
+	private int field3;			//private 접근 제한자
+	
+	// 생성자
+	public modifiers() {
+		field1 =1;
+		field2 =2;
+		field3 =3;
 		
-		// 생성자
-		public Modifiers() {
-			field1 =1;
-			field2 =2;
-			field3 =3;
-			
-			method1();
-			method2();
-			method3();
-		}
-		
-		// 메서드
-		public void method1() {};		
-		void method2() {};			
-		private void method3() {};		
+		method1();
+		method2();
+		method3();
+	}
+	
+	// 메서드
+	public void method1() {};		//public 접근 제한자
+	void method2() {};				//default 접근 제한자
+	private void method3() {};	//private 접근 제한자
 }
--- example/chap01/src/sec06/ex06/Modifiers2.java
+++ example/chap01/src/sec06/ex06/Modifiers2.java
@@ -1,16 +1,16 @@
-package sec06.ex06;
+package sec06.ex06;	// modifiers클래스와 패키지가 동일
 
-public class Modifiers2 {
+public class modifiers2 {
 	
-	public Modifiers2() {
-		Modifiers modi = new Modifiers();
+	public modifiers2() {
+		modifiers modi = new modifiers();
 		modi.field1 =1;
 		modi.field2 =2;
 //		modi.field3 =3;			private 접근제한자 이므로 에러
 		
 		modi.method1();
 		modi.method2();
-//		modi.method3();			private 접근제한자 이므로 에러
+//		modi.method3();		private 접근제한자 이므로 에러
 	}
 	
 }
