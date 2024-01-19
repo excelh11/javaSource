2385152eed9ba11f0c3b034f1584a6225cb37ef9
excelh11 <87971674+excelh11@users.noreply.github.com>
2024-01-19 12:25:16 +0900

    Update OuterClass3Ex.java

--- example/chap02/src/sec09/ex01/OuterClass3Ex.java
+++ example/chap02/src/sec09/ex01/OuterClass3Ex.java
@@ -7,7 +7,7 @@ public class OuterClass3Ex {
 
 	public static void main(String[] args) {
 		
-        // 정적 중첩 클래스의 인스턴스 생성
+        	// 정적 중첩 클래스의 인스턴스 생성
 		 StaticInnerClass staticInnerObj = new StaticInnerClass();
 		 
 		// 정적 중첩 클래스의 정적 메소드 호출
