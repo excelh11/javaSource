45be7d598e295c5e5cc450cd3148acf72d4b61ec
excelh11 <87971674+excelh11@users.noreply.github.com>
2024-01-19 13:58:57 +0900

    Update ParentEx.java

--- example/chap02/src/sec09/ex03/ParentEx.java
+++ example/chap02/src/sec09/ex03/ParentEx.java
@@ -3,7 +3,7 @@ package sec09.ex03;
 public class ParentEx {
 
 	public static void main(String[] args) {
-		 Parent parent = new Parent() {
+	     Parent parent = new Parent() {
 	            @Override
 	            public void doSomething() {
 	                System.out.println("익명 자식 클래스 입니다.");
