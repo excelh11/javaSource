e723cb6977e4ce25eb831cb2a3ce8bc903974e43
excelh11 <87971674+excelh11@users.noreply.github.com>
2024-01-19 12:50:23 +0900

    Update MathUtils.java

--- example/chap02/src/sec09/ex03/MathUtils.java
+++ example/chap02/src/sec09/ex03/MathUtils.java
@@ -4,5 +4,5 @@ public class MathUtils {
 	 public static void performCalculation(int x, int y, Calculator calculator) {
 	        int result = calculator.calculate(x, y);
 	        System.out.println("Result: " + result);
-	    }
+	 }
 }
