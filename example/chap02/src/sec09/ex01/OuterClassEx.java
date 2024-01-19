d6cdae7d30e003ccf433b41cbc5c310694b9f2cd
excelh11 <87971674+excelh11@users.noreply.github.com>
2024-01-19 12:13:46 +0900

    Update OuterClassEx.java

--- example/chap02/src/sec09/ex01/OuterClassEx.java
+++ example/chap02/src/sec09/ex01/OuterClassEx.java
@@ -3,8 +3,8 @@ package sec09.ex01;
 public class OuterClassEx {
 
 	public static void main(String[] args) {
-		  OuterClass outer = new OuterClass();
-	      outer.run();
+		OuterClass outer = new OuterClass();
+		outer.run();
 	}
 
 }
