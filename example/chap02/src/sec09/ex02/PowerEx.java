6186d5afaa3128acde2269f6915d25bd287dd9bf
excelh11 <87971674+excelh11@users.noreply.github.com>
2024-01-19 12:47:02 +0900

    Update PowerEx.java

--- example/chap02/src/sec09/ex02/PowerEx.java
+++ example/chap02/src/sec09/ex02/PowerEx.java
@@ -4,13 +4,13 @@ public class PowerEx {
 
 	public static void main(String[] args) {
 		
-		 PowerController powerController = new PowerController();
+		PowerController powerController = new PowerController();
 
 		// 전원 켜지면서 인디케이터 상태 확인
 	        powerController.turnOn();
 	        powerController.showPowerOn();
 
-	     // 전원 꺼지면서 인디케이터 상태 확인
+	    	// 전원 꺼지면서 인디케이터 상태 확인
 	        powerController.turnOff();
 
 	}
