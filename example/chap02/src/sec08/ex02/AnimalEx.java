1a54beac0f2da0b74e7f72e9363db0daadd3c7eb
excelh11 <87971674+excelh11@users.noreply.github.com>
2024-01-19 11:56:05 +0900

    Update AnimalEx.java

--- example/chap02/src/sec08/ex02/AnimalEx.java
+++ example/chap02/src/sec08/ex02/AnimalEx.java
@@ -3,16 +3,18 @@ package sec08.ex02;
 public class AnimalEx {
 
 	public static void main(String[] args) {
-		
-		    AnimalSound animalSound = new AnimalSound();
-
-	        Animal dog = new Dog();
-	        Animal cat = new Cat();
-
+		AnimalSound animalSound = new AnimalSound();
+	
+		Dog dog = new Dog();
+	        Cat cat = new Cat();
+	
+	       
 	        // 묵시적 변환
 	        // Animal animal = dog;
+	        dog.eat();
 	        animalSound.makeSound(dog);
 	        animalSound.makeSound(cat);
 	}
 
 }
+
