1e24f8046ecaca458be5dad963869bf80ef52a20
excelh11 <87971674+excelh11@users.noreply.github.com>
2024-01-22 16:18:08 +0900

    Update MemberEx.java

--- example/chap02/src/sec11/ex01/MemberEx.java
+++ example/chap02/src/sec11/ex01/MemberEx.java
@@ -13,39 +13,39 @@ public class MemberEx {
         
 	}
 }
-	class Member {
+class Member {
 	    private String name;
 	    private int age;
-
+	
 	    public Member(String name, int age) {
-	        this.name = name;
-	        this.age = age;
+		this.name = name;
+		this.age = age;
 	    }
-   
-
-		@Override
-		public String toString() {
-			return "Member [name=" + name + ", age=" + age + "]";
-		}
 
 
-		@Override
-	    public boolean equals(Object obj) {
+	@Override
+	public String toString() {
+		return "Member [name=" + name + ", age=" + age + "]";
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
 	       if(obj instanceof Member) {				// 매개값이 Person 타입인지 확인합니다
-	    	   Member member = (Member) obj;		// Person 타입으로 변환하고 
-	    	   if(name.equals(member.name)) {		// name필드값이 동일한지 검사 후
-	    		   return true;						// 동일 하면 true를 반환합니다
-	    	   }
+		   Member member = (Member) obj;		// Person 타입으로 변환하고 
+		   if(name.equals(member.name)) {		// name필드값이 동일한지 검사 후
+			   return true;						// 동일 하면 true를 반환합니다
+		   }
 	       }
 	       return false;							// 매개값이 Person 타입이 아니거나 
-	       											// name필드값이 다른 경우 false를 반환합니다
-	    }
-	    @Override
-	    public int hashCode() {
-	    	// name필드값이 null이 아니면 name.hashCode를 호출
-	        int result = name != null ? name.hashCode() : 0;
-	        result = 31 * result + age;
-	        return result;
-	    }
+												// name필드값이 다른 경우 false를 반환합니다
 	}
+        @Override
+        public int hashCode() {
+		// name필드값이 null이 아니면 name.hashCode를 호출
+		int result = name != null ? name.hashCode() : 0;
+		result = 31 * result + age;
+		return result;
+    	}
+}
 
