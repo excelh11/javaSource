11eb1eb0259b3aab5220e10fa8028801ec876026
excelh11 <87971674+excelh11@users.noreply.github.com>
2024-01-22 16:26:52 +0900

    Update ResouceEx.java

--- example/chap02/src/sec11/ex01/ResouceEx.java
+++ example/chap02/src/sec11/ex01/ResouceEx.java
@@ -9,24 +9,24 @@ public class ResouceEx {
 
 	public static void main(String[] args) {
 		 // 현재 클래스의 클래스 로더를 사용하여 리소스 로드
-        URL resourceUrl = Member.class.getResource("data.txt");
-        if (resourceUrl != null) {
-            System.out.println("Resource URL: " + resourceUrl);
-            
-            // 리소스 읽기
-            try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(resourceUrl.openStream()));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
-                }
-                reader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("Resource not found.");
-        }
+	        URL resourceUrl = Member.class.getResource("data.txt");
+	        if (resourceUrl != null) {
+	            System.out.println("Resource URL: " + resourceUrl);
+	            
+	            // 리소스 읽기
+	            try {
+	                BufferedReader reader = new BufferedReader(new InputStreamReader(resourceUrl.openStream()));
+	                String line;
+	                while ((line = reader.readLine()) != null) {
+	                    System.out.println(line);
+	                }
+	                reader.close();
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        } else {
+	            System.out.println("Resource not found.");
+	        }
 	}
 
 }
