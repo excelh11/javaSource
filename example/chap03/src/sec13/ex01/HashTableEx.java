adb84f2397fd7f19cfcdf20d48856b7d82accecc
excelh11 <87971674+excelh11@users.noreply.github.com>
2024-01-24 19:58:12 +0900

    Update HashTableEx.java

--- example/chap03/src/sec13/ex01/HashTableEx.java
+++ example/chap03/src/sec13/ex01/HashTableEx.java
@@ -5,38 +5,53 @@ import java.util.Hashtable;
 public class HashTableEx {
 
 	public static void main(String[] args) {
-		// Hashtable 생성 (키: 문자열, 값: 정수)
+		 // Hashtable 생성 (키: 문자열, 값: 정수)
         Hashtable<String, Integer> hashtable = new Hashtable<>();
+
+        // 객체를 사용하여 동기화
+        Object lock = new Object();
 
         // 데이터 추가하는 스레드 생성
         Thread addThread = new Thread(() -> {
-            for (int i = 1; i <= 5; i++) {
-                hashtable.put("key" + i, i);
-                System.out.println("추가: key" + i + " -> " + i);
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            synchronized (lock) {
+                for (int i = 1; i <= 5; i++) {
+                    while (hashtable.containsKey("key" + i)) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    hashtable.put("key" + i, i);
+                    System.out.println("추가: key" + i + " -> " + i);
+                    lock.notifyAll();
                 }
             }
         });
 
         // 데이터 조회하는 스레드 생성
         Thread getThread = new Thread(() -> {
-            for (int i = 1; i <= 5; i++) {
-                int value = hashtable.get("key" + i);
-                System.out.println("조회: key" + i + " -> " + value);
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            synchronized (lock) {
+                for (int i = 1; i <= 5; i++) {
+                    while (!hashtable.containsKey("key" + i)) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    int value = hashtable.get("key" + i);
+                    System.out.println("조회: key" + i + " -> " + value);
+                    lock.notifyAll();
                 }
             }
         });
 
         // 스레드 시작
-        addThread.start();
         getThread.start();
+        addThread.start();
 
         // 메인 스레드가 두 스레드의 작업을 기다림
         try {
