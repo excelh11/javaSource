98c113a8601a3bed16df5f41d9f9bf5801b316fe
excelh11 <87971674+excelh11@users.noreply.github.com>
2024-01-25 11:00:56 +0900

    Update MehtodEx.java

--- example/chap03/src/sec14/ex01/MehtodEx.java
+++ example/chap03/src/sec14/ex01/MehtodEx.java
@@ -1,6 +1,6 @@
 package sec14.ex01;
 import java.util.function.Function;
-public class MehtodEx {
+public class MethodEx {
 
 		public static void main(String[] args) {
 	        Function<String, Integer> parseInt = Integer::parseInt;
